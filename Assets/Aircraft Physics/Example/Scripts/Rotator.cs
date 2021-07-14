79be6d291b79968587b20acbcbe2c2f85b8bf721
salomaogabriel <65493468+salomaogabriel@users.noreply.github.com>
2021-07-13 23:06:35 -0300

    Smooth transition between speed changes
    I added a speed transition so the rotation looks more real. It can use a quadratic equation to make it smoothier but it works for now

--- Assets/Aircraft Physics/Example/Scripts/Rotator.cs
+++ Assets/Aircraft Physics/Example/Scripts/Rotator.cs
@@ -3,9 +3,24 @@
 public class Rotator : MonoBehaviour
 {
     public float speed;
+        public float curSpeed;
 
     private void Update()
     {
         transform.localRotation *= Quaternion.AngleAxis(speed * Time.deltaTime , Vector3.up);
+          if(curSpeed != speed)
+        {
+            ChangeSpeed();
+        }
+    }
+      private void ChangeSpeed()
+    {
+        if(curSpeed > speed)
+        {
+            curSpeed -=1;
+        }
+        else {
+            curSpeed +=1;
+        }
     }
 }
